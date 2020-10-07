69665c3fa6da2651af50fc2434579cdeb8459fab
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-10-07 05:59:40 +0000

    Put in a tweet section in the body of the main page and styled it

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,9 +7,11 @@ $(document).ready(function() {
   $('#tweet-text').on('input', (function() {
     let textInput = $(this).val()
     //console.log(140 - (textInput.length));
-
-    $(".counter").text(140 - (textInput.length));
-
+    if (textInput.length < 140) {
+    $(".counter").css("color", "black").text(140 - (textInput.length))
+    } else if (textInput.length >= 140) {
+      $(".counter").css("color", "red").text(140-(textInput.length))
+    }
   }))
 });
 
