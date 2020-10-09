434f8cca5a3ed384523f053ba6ca4cf5eaace371
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-10-09 23:59:49 +0000

    Fixed bugs occuring in the createTweetElement function and put in some stylized alert messages

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
+  
+  //error function 
+  const $error= $('.alert')
+  const showError = function (errorMessage) {
+    $error.slideDown ("fast");
+    $error.css("display", "flex");
+    $('.alert-message').html(errorMessage)
+  }
 
+  // function to render tweets
   const loadTweets = function () {
     $.ajax('/tweets/', { 
       method: 'GET',
@@ -17,115 +26,99 @@ $(document).ready(function() {
 
   loadTweets();
 
+
+
   //When I submit a tweet it adds it to the server database
   $('#tweet-form').submit(function (event) {
     event.preventDefault();
-  //   console.log("sending tweets")
-  //  console.log(event.target.text.value)
-  //  console.log($(this).serialize())
-    if ($("#tweet-text").val().length === 0) {
-      alert ("Please enter a message before hitting submit")
-    } else if ($("#tweet-text").val().length > 140) {
-      alert("Your tweet is greater than 140 characters. Please try again")
+    const theTweetText = $("#tweet-text").val()
+
+    if (theTweetText.length === 0) {
+      showError("Hey! We can't do all the work for you. Tell us what you're thinking!")
+
+    } else if (theTweetText.length > 140) {
+      showError("Woah, calm down there Shakespeare... your message is too long")
+
     } else {
-    $.ajax({
-      method: "POST", 
-      url: "/tweets/",
-      //data: {"text": $("#tweet-text").val()} // same as below
-      data: $(this).serialize()
-    })
+      $error.slideUp("slow");
+
+      //function to sanitize form input 
+      // const escape =  function(str) {
+      //   let div = document.createElement('div');
+      //   div.appendChild(document.createTextNode(str));
+      //   return div.innerHTML;
+      // }
+
+      // console.log(theTweetText)
+      // const safeText = escape(theTweetText);
+      // console.log(safeText)
+    
+
+      //console.log($(this).serialize())
+      $.ajax({
+        method: "POST", 
+        url: "/tweets/",
+        //data: {"text": $("#tweet-text").val()} // same as below
+        //data: {"text": safeText}
+        data: $(this).serialize()
+      })
     .then (function () {
-      console.log("sucessfully submitted!")
-      $(".tweets").empty()
+      //console.log("sucessfully submitted!")
+
       //$("#tweet-form").reset() //<- this doesnt actually work because reset is not in jquery
       document.getElementById('tweet-form').reset()
       $(".counter").html(140)
+      $(".tweets").empty()
       loadTweets();
     })
   }
   });
 
-
   const renderTweets = function (tweets) {
-    //loops through tweets - takes in an array of tweet objects
-    //calls createTweetElement for each tweet
-    //takes return value and appends it to the tweets container
+    //const reverseTweets = tweets.reverse();
     for (let tweet of tweets) {
       const $tweetElement = createTweetElement(tweet);
-      $('.container').append($tweetElement);
+      $('.tweets').prepend($tweetElement);
     } 
   }
 
   const createTweetElement = function (tweet) {
-    const $tweet = $(`      <section class="tweets">
-    <article>
-      <header class="tweet-header">
-        <div class="avatar-name">
-          <div class="avatar">
-            <img src= "https://i.imgur.com/73hZDYK.png" alt="Avatar of old man">
-          </div>
-          <div class="tweet-name">
-            ${tweet.user.name}
-          </div>
-        </div>
-       
-      <div class="tweet-handle">
-          ${tweet.user.handle}
-        </div>
-    </header>
+      const escape =  function(str) {
+        let div = document.createElement('div');
+        div.appendChild(document.createTextNode(str));
+        return div.innerHTML;
+      }
 
-    <p class="tweet">
-      ${tweet.content.text}
-    </p>   
-    <hr>
-    <footer class="tweet-footer">
-      <div>
-        ${tweet["created_at"]}
+    return `<article>
+    <header class="tweet-header">
+      <div class="avatar-name">
+        <div class="avatar">
+          <img src= ${tweet.user.avatars}>
+        </div>
+        <div class="tweet-name">
+          ${tweet.user.name}
+        </div>
       </div>
-      <div>
-        &#127988 &#x2794 &#10084
+     
+    <div class="tweet-handle">
+        ${tweet.user.handle}
       </div>
-    </footer>
+  </header>
 
-    </article>
-  </section>`)
-    return $tweet;
+  <p class="tweet">
+    ${escape(tweet.content.text)}
+  </p>   
+  <hr>
+  <footer class="tweet-footer">
+    <div>
+      10 days ago
+    </div>
+    <div>
+      &#127988 &#x2794 &#10084
+    </div>
+  </footer>
+
+  </article>`
   }
 
   })
-
-// $( "#input-tweet" ).submit(function( event ) {
-//   alert( "Handler for .submit() called." );
-//   event.preventDefault();
-// });
-
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
-
-//  "text": $("#tweet-text") < this is yøur THIS
-    // .then(function (textField) {
-    //   const textField = $('#tweet-text').val();
-    //   console.log ('Succceess!!: ', textField);
